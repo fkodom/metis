9f86601509adfccf3f4db9efa89e6d914afbb39d
Frank Odom <frank.odom.iii@gmail.com>
2020-08-17 23:05:38 -0500

    Added default agents, updated examples to use default models.

--- examples/actor_critic.py
+++ examples/actor_critic.py
@@ -5,11 +5,12 @@ import torch
 
 from metis import agents, play
 from metis import trainers
+from metis.trainers import SAC
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--env", default="CartPole-v1")
-parser.add_argument("--trainer", default="DQN")
+parser.add_argument("--trainer", default="PPO")
 parser.add_argument("--cuda", action="store_true")
 args = parser.parse_args()
 
@@ -17,8 +18,12 @@ env = gym.make(args.env)
 device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
 
 trainer = getattr(trainers, args.trainer)(env)
-actor = agents.default_dqn(env, trainer)
-trainer.train(actor)
+actor = agents.default_actor(env, trainer).to(device)
+critics = [
+    agents.default_critic(env, trainer).to(device), 
+    agents.default_critic(env, trainer).to(device),
+]
+trainer.train(actor, critics)
 
 for i in range(100):
     play(env, actor)
